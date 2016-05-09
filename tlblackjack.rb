121c59a62694c43b74065565160ce4a2a9a16e00
Oscar Daniels <oscardaniels@gmail.com>
2016-05-09 16:41:21 -0400

    blackjack oo

---
+++ tlblackjack.rb
@@ -0,0 +1,229 @@
+class Card
+  attr_accessor :suit, :face_value
+
+  def initialize(s, fv)
+    @suit = s
+    @face_value = fv
+  end
+
+  def pretty_output
+    "The #{face_value} of #{find_suit}"
+  end
+
+  def to_s
+    pretty_output
+  end
+
+  def find_suit
+    ret_val = case suit
+                when 'H' then 'Hearts'
+                when 'D' then 'Diamonds'
+                when 'S' then 'Spades'
+                when 'C' then 'Clubs'
+              end
+    ret_val
+  end
+end
+
+class Deck
+  attr_accessor :cards
+  def initialize
+    @cards = []
+    ['H', 'D', 'S', 'C'].each do |suit|
+      ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']. each do |face_value|
+        @cards << Card.new(suit, face_value)
+      end
+    end
+    scramble
+  end
+
+  def scramble
+    cards.shuffle!
+  end
+  
+  def deal_one
+    cards.pop
+  end
+
+  def size
+    cards.size
+  end
+end
+
+module Hand
+  def show_hand
+    puts "---- #{name}'s Hand ----"
+    cards.each do |card|
+      puts "=> #{card}"
+    end
+    puts "=> Total: #{total}"
+    puts
+  end
+
+  def total
+    face_values = cards.map{|card| card.face_value}
+
+    total = 0
+    face_values.each do |val|
+      if val == "A"
+        total += 11
+      else
+        total += (val.to_i == 0 ? 10 : val.to_i)
+      end
+    end
+    face_values.select{|val| val == "A"}.count.times do 
+      break if total <= 21
+      total -= 10
+    end
+    total
+  end
+
+  def add_card(new_card)
+    cards << new_card
+  end
+
+  def busted?
+    total > 21
+  end
+
+  def blackjack?
+    total == 21
+  end
+
+  def hit_or_stay
+    choice = ""
+    loop do
+      puts "Would you like to hit or stay? (h/s)"
+      choice = gets.chomp.downcase
+      break if ['h','s'].include?(choice)
+    end
+    choice
+  end
+end
+
+class Player
+  include Hand
+
+  attr_accessor :name, :cards
+
+  def initialize(n)
+    @name = n
+    @cards = []
+  end
+
+  def take_turn(deck)
+    loop do
+      if hit_or_stay == 'h'
+        add_card(deck.deal_one)
+        show_hand
+      else 
+        break
+      end
+      break if busted? || blackjack?
+    end
+  end
+
+end
+
+class Dealer
+  include Hand
+  attr_accessor :name, :cards, :hide_hole_card
+
+  def initialize
+    @name = "Dealer"
+    @cards = []
+    @hide_hole_card = true
+  end
+
+  def show_hand
+    if hide_hole_card == true
+      puts "---- #{name}'s Hand ----"
+      puts "=> The ?? of ?????"
+      puts "=> #{cards.first}"
+      puts '=> Total: ??'
+      self.hide_hole_card = false
+    else
+      puts "---- #{name}'s Hand ----"
+      cards.each do |card|
+        puts "=> #{card}"
+      end
+      puts "=> Total: #{total}"
+    end
+  end
+
+  def take_turn(deck)
+    loop do
+      if total < 17
+        add_card(deck.deal_one)
+        show_hand
+        sleep 2
+      else 
+        break
+      end
+      break if busted? || blackjack?
+    end
+  end
+
+end
+
+
+
+class Game
+  attr_accessor :deck
+  attr_reader :player, :dealer
+  
+  def initialize(n)
+    @deck = Deck.new
+    @player = Player.new(n)
+    @dealer = Dealer.new
+  end
+
+  def initial_deal
+    player.add_card(deck.deal_one)
+    player.add_card(deck.deal_one)
+    dealer.add_card(deck.deal_one)
+    dealer.add_card(deck.deal_one)
+  end
+
+  def play
+    system "cls"
+    initial_deal
+    player.show_hand
+    dealer.show_hand
+    player.take_turn(deck)
+    if !player.busted?
+      dealer.take_turn(deck)
+    end
+    results
+  end
+
+  def results
+    puts "#{player.name}: #{player.total} Dealer: #{dealer.total}"
+    case 
+    when player.busted? then puts "You busted. Dealer wins."
+    when dealer.busted? then puts "Dealer busted! You win."
+    when player.total > dealer.total then puts "#{player.name} wins."
+    when dealer.total > player.total then puts "Dealer wins."
+    when dealer.total == player.total then puts "It's a push."
+    end
+  end
+
+  def play_again
+    puts "Would you like to play again? (y/n)"
+    gets.chomp.downcase
+  end
+end
+
+def get_name
+  puts "What is your name?"
+  name = gets.chomp
+end
+
+name = get_name
+loop do
+  game = Game.new(name)
+  game.play
+  break unless game.play_again == 'y'
+end
+
+
